257ecd97ebf5569eefdbdf04ddb605d811e4e6fc
Ben Anderman <ben@happyspork.com>
2019-04-26 18:02:27 -0400

    Add procfile; set port/host for heroku

--- application.py
+++ application.py
@@ -1,3 +1,5 @@
+import os
+
 from degrading_pastebin import app
 import degrading_pastebin.pastebin
 
@@ -5,6 +7,7 @@ if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     app.debug = True
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
